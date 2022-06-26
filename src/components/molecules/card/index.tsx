fe54fe336bec1b4a2ca862a4cea44cc43b479964
ryunosuke0922 <ryumaeda0922@gmail.com>
2022-06-26 23:47:04 +0900

    fix

--- src/components/molecules/card/index.tsx
+++ src/components/molecules/card/index.tsx
@@ -8,20 +8,26 @@ type Props = {
 const EnhancedCard = ({ data }: Props): JSX.Element => {
   return (
     <Card>
-      <TextBox>
-        <div>
-          <TextSmall>{data.no}</TextSmall>
-        </div>
-        <div>
-          <TextSmall>{data.prefectures}</TextSmall>
-          <TextSmall>{data.mountainSystem}</TextSmall>
-          <TextSmall>標高：{data.elevation} m</TextSmall>
-        </div>
-        <div>
-          <TextBig>{data.name}</TextBig>
-          <TextBig>{data.kanaName}</TextBig>
-        </div>
-      </TextBox>
+      <a
+        href={`https://yamap.com/search/activities?keyword=${data.prefectures}%20${data.name}`}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <TextBox>
+          <div>
+            <TextSmall>{data.no}</TextSmall>
+          </div>
+          <div>
+            <TextSmall>{data.prefectures}</TextSmall>
+            <TextSmall>{data.mountainSystem}</TextSmall>
+            <TextSmall>標高：{data.elevation} m</TextSmall>
+          </div>
+          <div>
+            <TextBig>{data.name}</TextBig>
+            <TextBig>{data.kanaName}</TextBig>
+          </div>
+        </TextBox>
+      </a>
     </Card>
   )
 }
