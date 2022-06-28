ac34b00ce263059d022a6d3950a307b89cae52e6
ryunosuke0922 <ryumaeda0922@gmail.com>
2022-06-29 01:16:24 +0900

    fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,16 @@
 import usePageTracking from '@/useTracking'
+import { AnimatePresence } from 'framer-motion'
 import 'modern-css-reset/dist/reset.min.css'
 import '../styles/globals.css'
 import '../styles/Home.module.css'
 
 function MyApp({ Component, pageProps }) {
   usePageTracking()
-  return <Component {...pageProps} />
+  return (
+    <AnimatePresence>
+      <Component {...pageProps} />
+    </AnimatePresence>
+  )
 }
 
 export default MyApp
