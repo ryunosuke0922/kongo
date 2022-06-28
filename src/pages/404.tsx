99052959e015ef0754ff492dc165051f4282224e
ryunosuke0922 <ryumaeda0922@gmail.com>
2022-06-29 01:46:16 +0900

    no message

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,26 +1,50 @@
-import Footer from '@/components/organisms/footer/footer'
-import Header from '@/components/organisms/header/header'
-import { MainContent, Wrapper, WrapperContent } from '@/layouts/top/style'
 import type { NextPage } from 'next'
 import styled from 'styled-components'
 
 const Err = styled.div`
   background: #fafafa;
+  background: url(https://media.giphy.com/media/lJNoBCvQYp7nq/giphy.gif) no-repeat center center
+    fixed;
+  background-size: cover;
+  font-family: 'Roboto', sans-serif;
+  height: 1200px;
+  h1 {
+    font-size: 16em;
+    margin: 0.2em 0.5em;
+    color: rgba(255, 255, 255, 0.7);
+    margin-bottom: 0px;
+  }
+  h2 {
+    font-size: 1.7em;
+    margin: 0.2em 0.5em;
+    color: rgba(255, 255, 255, 0.6);
+  }
+
+  .error {
+    position: absolute;
+    top: 30%;
+    margin-top: -8em;
+    width: 100%;
+    text-align: center;
+  }
+
+  .material-icons {
+    font-size: 1.5em;
+    position: relative;
+    top: 10px;
+  }
 `
 const Err404: NextPage = () => {
   return (
     <Err>
-      <Header />
-      <main className="main container">
-        <Wrapper>
-          <WrapperContent>
-            <MainContent>
-              <h1>404</h1>
-            </MainContent>
-          </WrapperContent>
-        </Wrapper>
-      </main>
-      <Footer />
+      {/* <Header /> */}
+      {/* <main className="main container"> */}
+      <div className="error">
+        <h1>404</h1>
+        <h2>Page not found</h2>
+      </div>
+      {/* </main> */}
+      {/* <Footer /> */}
     </Err>
   )
 }
